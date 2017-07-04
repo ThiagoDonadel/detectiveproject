c9015e3564c9aef8dba02f4f795ed8cdff9b01ba
Thiago Donadel <thiagodonadel2@gmail.com>
2017-07-04 17:39:07 -0300

    Mecanicas

---
+++ Assets/Scripts/GameScripts/Level/Mechanics/HideAndSeek.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HideAndSeek : MonoBehaviour {
+
+
+    public Spirit spirt;
+    public GameObject mirror;
+
+    // Use this for initialization
+    void Start() {
+        spirt.Move((Vector2)transform.position);
+        StartCoroutine(ShakeMirror());
+        //spirt.Move(new Vector2(7.5f,1.5f));
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        print("oi");
+    }
+
+    private IEnumerator ShakeMirror() {
+
+        for(int i=0;i<4;i++) {
+            mirror.transform.Rotate(0, 0, 10);
+            yield return new WaitForSeconds(0.5f);
+            mirror.transform.Rotate(0, 0, -20);
+            yield return new WaitForSeconds(0.5f);
+            mirror.transform.Rotate(0, 0, 10);
+        }
+        
+
+        yield return null;
+    }
+}
---
+++ Assets/Scripts/GameScripts/Level/Mechanics/Spirit.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spirit : MonoBehaviour {
+
+    public float speed = 2;
+    public string[] lines;
+
+    private bool moving;
+    private Vector2 targetPos;
+
+    private void Awake() {
+        moving = false;
+        ShowHide();
+    }
+
+    // Use this for initialization
+    void Start() {
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {       
+		if(moving) {
+            Rigidbody2D rb = GetComponent<Rigidbody2D>();
+            Vector2 direction = (targetPos - rb.position).normalized;
+            rb.MovePosition(rb.position + direction * speed * Time.deltaTime);
+           
+            if(Vector2.Distance(targetPos, rb.position) < 0.1f) {                
+                moving = false;
+                ShowHide();
+            }
+            
+        }
+	}
+   
+
+    public void ShowHide() {
+        GetComponent<SpriteRenderer>().enabled = moving;
+        //GetComponent<Collider2D>().enabled = moving;
+    }
+
+
+    public void Move(Vector2 destination) {        
+        targetPos = destination;
+        moving = true;
+        ShowHide();        
+    }
+
+}
