1e73237dfc68481a3f25643b8e8de90b755e6865
Thiago Donadel <thiagodonadel2@gmail.com>
2017-07-13 17:21:11 -0300

    Mecanicas

--- Assets/Scripts/GameScripts/Level/Mechanics/Cauldron.cs
+++ Assets/Scripts/GameScripts/Level/Mechanics/Cauldron.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Cauldron : IterativeObject {
 
@@ -8,19 +9,26 @@ public class Cauldron : IterativeObject {
     private int[] mixOrder;
 
     private bool preparingPotion;
+    private Canvas mixingCanvas;
+    private Transform mixingDialog;
+    private int next;
 
 	// Use this for initialization
 	void Start () {
         ingredients = new int[3] { 0, 0, 0 };
-        mixOrder = new int[3] { -1, -1, -1 };
+        mixOrder = new int[3] { 0, 0, 0 };
+        next = 0;
         preparingPotion = false;
+        mixingCanvas = transform.Find("MixPotionCanvas").GetComponent<Canvas>();
+        mixingDialog = mixingCanvas.transform.Find("MixPotionDialog");
+        mixingCanvas.enabled = false;
     }
 
     public override bool Interact(GameObject actor) {
         
         if(!preparingPotion) {
             if(CheckIngredients()) {
-                MixPotion();
+                PreparePotion();
             } else {
                 GUIController.instance.eventDialog.ShowDialog("Parece que alguns ingredientes estão faltando!!");
             }
@@ -30,11 +38,29 @@ public class Cauldron : IterativeObject {
     }
 
     public void PreparePotion() {
-        preparingPotion = true;
+        if (!preparingPotion) {
+            preparingPotion = true;
+            mixingDialog.GetComponentInChildren<Text>().text = "Os ingredientes estão no caldeirão!! Agora é necessário mecher a poção.. Escolha um sentido para mecher";
+            mixingCanvas.enabled = true;
+        } else {
+            if(next >= mixOrder.Length) {
+                CheckPrepareResult();
+            } else {             
+                mixingDialog.GetComponentInChildren<Text>().text = next == mixOrder.Length - 1 ? "Vamos lá ... mecha uma ultima vez..." : "Continue a mecher... ";
+                mixingCanvas.enabled = true;
+            }          
+        }
     }
 
-    public void MixPotion() {
-        
+    private void CheckPrepareResult() {
+
+    }
+
+    public void MixPotion(int side) {
+        mixingCanvas.enabled = false;
+        mixOrder[next] = side;
+        next++;
+        PreparePotion();
     }
 
 
